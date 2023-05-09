52e2c157d68ad3bf22078b51363f860e2c6649a4
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-05-09 14:43:26 -0400

    feat: get config working

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,31 @@
-use safelog::{sensitive, Sensitive};
+use safelog::Sensitive;
+use serde::Deserialize;
+use std::collections::HashMap;
 use std::env;
+use std::fs;
 
+#[derive(Deserialize, Debug)]
 struct Config {
     group_id: u64,
     roblosecurity: Sensitive<String>,
+    scanned_ranks: Vec<String>,
+    rank_year_pairs: HashMap<String, Vec<u64>>,
+}
+
+#[derive(Debug, Clone, thiserror::Error)]
+enum Error {
+    #[error("Please provide a config file as an argument. Usage: classics-ranking-bot.exe <config_file>")]
+    ConfigFileNotProvided,
 }
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let config_file = match env::args().nth(1) {
-        Some(file) => file,
-        None => {
-            eprintln!("Please provide a config file as an argument. Usage: classics-ranking-bot.exe <config_file>");
-            return;
-        }
+        Some(file_path) => serde_json::from_str::<Config>(&fs::read_to_string(file_path)?)?,
+        None => return Err(Error::ConfigFileNotProvided.into()),
     };
 
-    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    dbg!(config_file);
+
+    Ok(())
 }
